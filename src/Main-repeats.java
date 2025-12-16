71f932c7f146e5b39fb2b9b5fb33bcce45313623
Oksana (Kes) Milenina <OkMilenina@yandex.ru>
2025-12-16 12:23:39 +0300

    Тренажер на повторение символов в строке

---
+++ src/Main-repeats.java
@@ -0,0 +1,22 @@
+﻿// для тренажера
+public class Main {
+    public static final String TEXT = "aaababaabaaaabaabaabaabaaababaabaaababaabaaaabaabaabaabbabaabaaababaababaabaabaabaaabbaab";
+    public static final String PATTERN = "aab";
+
+    public static void main(String[] args) {
+        int count = 0;
+
+        for (int i = 0; i < TEXT.length() - PATTERN.length() + 1; i++) {
+            boolean match = true;
+            for (int j = 0; j < PATTERN.length(); j++){
+                if (TEXT.charAt(i + j) != PATTERN.charAt(j)) {
+                    match = false;
+                    break;
+                }
+            }
+            if (match) count++;
+        }
+
+        System.out.println("Строка " + PATTERN + " встретилась в тексте " + count + " раз");
+    }
+}
